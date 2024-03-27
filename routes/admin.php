cae09e5fefa6d0d8b603243965cfaba11bc90bd6
agungkusaeri9 <agung.kusaeri9@gmail.com>
2024-03-27 08:53:55 +0700

    fix: akses role keuangan ke keuangan

--- routes/admin.php
+++ routes/admin.php
@@ -49,10 +49,10 @@ Route::middleware('cek_role:admin')->group(function () {
     });
 
     Route::resource('transaksi', TransaksiController::class);
-    Route::resource('keuangan', KeuanganController::class);
-    Route::post('/laporankeuangan', [KeuanganController::class, 'print'])->name('keuangan.print');
 });
 
+Route::resource('keuangan', KeuanganController::class);
+Route::post('/laporankeuangan', [KeuanganController::class, 'print'])->name('keuangan.print');
 Route::controller(LaporanController::class)->name('laporan.')->group(function () {
     Route::get('/laporan/transaksi', 'transaksi_index')->name('transaksi.index')->middleware('cek_role:admin');
     Route::post('/laporan/transaksi', 'transaksi_print')->name('transaksi.print')->middleware('cek_role:admin');
